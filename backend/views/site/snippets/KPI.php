eb9678ee6068dde0ff4d0963af725c62c5e61371
Franchic211 <imagine.innovation@outlook.fr>
2026-02-28 16:47:44 +0100

    Restore deleted files

--- backend/views/site/snippets/KPI.php
+++ backend/views/site/snippets/KPI.php
@@ -1,51 +1,21 @@
 <?php
+
+use backend\helpers\KpiHelper;
+
 /** @var yii\web\View $this */
-$this->title = 'My Yii Application XX';
 ?>
-<div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+<div class="row row-cols-1 row-cols-md-<?= KpiHelper::mdBreakpoint() ?> row-cols-lg-<?= KpiHelper::lgBreakpoint() ?> row-cols-xxl-<?= KpiHelper::xxlBreakpoint() ?> g-3 mb-4">
+    <?php foreach (KpiHelper::KPI as $kpi): ?>
+        <div class="col">
+            <div class="card card-kpi <?= $kpi['backgroundStyle'] ?> bg-gradient text-white border-0 shadow-sm">
+                <div class="card-body">
+                    <h6 class="card-subtitle mb-2 opacity-75"><?= $kpi['title'] ?></h6>
+                    <h2 class="card-title mb-0">
+                        <span id="<?= $kpi['containerName'] ?>">?</span>
+                    </h2>
+                    <i class="bi <?= $kpi['icon'] ?> kpi-icon"></i>
+                </div>
             </div>
         </div>
-
-    </div>
+    <?php endforeach; ?>
 </div>
