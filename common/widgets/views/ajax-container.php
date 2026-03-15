986f355b26769ca8f87139b5362e94942cb0233d
Franchic211 <imagine.innovation@outlook.fr>
2026-03-15 20:10:25 +0100

    frontend\web\img directory moved to common\web\img to share images with backend. Created WebResoucesHelper to manage access to images

--- common/widgets/views/ajax-container.php
+++ common/widgets/views/ajax-container.php
@@ -1,14 +1,18 @@
 <?php
+
+use common\helpers\WebResourcesHelper;
+
 /** @var string|null $tag */
 /** @var string|null $name */
 /** @var string|null $param */
+$imgPath = WebResourcesHelper::imagePath();
 $tag = $tag ?? 'div';
 ?>
 <<?= $tag ?> id="<?= $name ?>" <?= $param ?>>
 <div class="text-center">
     <div class="spinner-border" role="status" style="width: 3rem; height: 3rem;">
         <span>
-            <img src="/frontend/web/img/Dragonfly32White.png" alt="">
+            <img src="<?= $imgPath ?>/Dragonfly32White.png" alt="">
         </span>
     </div>
 </div>
