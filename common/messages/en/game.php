bc536bef280587f563efa3bd9fc518871c77a892
Franchic211 <imagine.innovation@outlook.fr>
2026-08-04 11:34:31 +0200

    Feat: remove unnecessary messages

--- common/messages/en/game.php
+++ common/messages/en/game.php
@@ -1,38 +1,40 @@
 <?php
+/**
+ * Special character for english:
+ *  I'm become I’m
+ *  "London" become “London”
+ */
 return [
-    'player succeeded action' => '{playerName} successfully completed the “{actionName}” action',
-    'player failed action' => '{playerName} partially completed the “{actionName}” action',
-    'player partially succeeded action' => '{playerName} failed to complete the “{actionName}” action',
-    'missing item for action by player' => '{playerName} tried “{actionName}” but was missing a required item',
-    'player action' => '{playerName} completed the “{actionName}” action with an unknown status',
     'rolling dice result' => 'Rolling {diceToRoll} gave {diceRoll}',
-    'the action partialy succeeded' => 'the action partialy succeeded',
-    'the action succeeded' => 'the action succeeded',
-    'the action failed' => 'the action failed',
-    'the action did something' => 'something happened, but I don\'t know what',
     'loosing hp' => '{hpLoss, plural,
-        =0 {You haven\'t lost any Hit Points}
+        =0 {You haven’t lost any Hit Points}
         one {You lost one hit point}
         other {You lost # hit points}
     }',
     'gained gp' => '{gp, plural,
-        =0 {You haven\'t gain any gold pieces}
+        =0 {You haven’t gain any gold pieces}
         one {You gained one gold piece}
         other {You gained # gold pieces!}
     }',
     'gained xp' => '{xp, plural,
-        =0 {You haven\'t gain any experience point}
+        =0 {You haven’t gain any experience point}
         one {You gained one experience point}
         other {You gained # experience points!}
     }',
-    'Something happened' => 'Something happened, that\'s for sure, but I don\'t really know what',
-    'action_status' => '{status, select,
+    'Something happened' => 'Something happened, that’s for sure, but I don’t really know what',
+    'action status' => '{status, select,
         SUCCESS {{playerName} successfully performed “{actionName}”}
         PARTIAL {{playerName} partially succeeded in “{actionName}”}
         FAILURE {{playerName} failed to perform “{actionName}”}
-        unknown {It is unknown whether {playerName} succeeded in “{actionName}”}
         ITEM_MISSING {{playerName} is missing an item to perform the action “{actionName}”}
         other {It is unknown whether {playerName} succeeded in “{actionName}”}
+    }',
+    'simple action status' => '{status, select,
+        SUCCESS {The action was successful}
+        PARTIAL {The action was partially successful}
+        FAILURE {The action failed}
+        ITEM_MISSING {An object is missing to perform the action}
+        other {I don’t know whether the action was successful or not}
     }',
     'gained item' => 'You now have a {itemName} in your back bag',
 ];
--- common/messages/fr/game.php
+++ common/messages/fr/game.php
@@ -1,15 +1,11 @@
 <?php
+/**
+ * Special character for french:
+ *  C'est become C’est
+ *  "Paris" become « Paris »
+ */
 return [
-    'player succeeded action' => '{playerName} a réussi l\'action {actionName}',
-    'player failed action' => '{playerName} a échoué à réaliser l\'action {actionName}',
-    'player partially succeeded action' => '{playerName} a partiellement réussi l\'action {actionName}',
-    'missing item for action by player' => 'Il manquait un objet à {playerName} pour qu\'il réaliser l\'action {actionName}',
-    'player action' => '{playerName} a tenté l\'action {actionName}',
     'rolling dice result' => 'Le lancer de {diceToRoll} a donné {diceRoll}',
-    'the action partialy succeeded' => 'l\'action a partiellement réussi',
-    'the action succeeded' => 'l\'action a réussi',
-    'the action failed' => 'l\'action a échoué',
-    'the action did something' => 'il s\'est passé quelque chose, mais je ne sais pas quoi',
     'loosing hp' => '{hpLoss, plural,
         =0 {Tu n’as perdu aucun point de vie}
         one {Tu as perdu un point de vie}
@@ -25,13 +21,20 @@ return [
         one {Tu as gagné un point d’expérience}
         other {Tu as gagné # points d’expérience}
     } !',
-    'Something happened' => 'Il s\'est passé quelque chose, ça c\'est certain, mais je ne sais pas vraiment quoi',
-    'action_status' => '{status, select,
-        SUCCESS {{playerName} a réussi l’action «&nbsp;{actionName}&nbsp;»}
-        PARTIAL {{playerName} a partiellement réussi l’action «&nbsp;{actionName}&nbsp;»}
-        FAILURE {{playerName} a échoué dans l’action «&nbsp;{actionName}&nbsp;»}
-        ITEM_MISSING {Il manque un objet à {playerName} pour réaliser l’action «&nbsp;{actionName}&nbsp;»}
+    'Something happened' => 'Il s’est passé quelque chose, ça c’est certain, mais je ne sais pas vraiment quoi',
+    'action status' => '{status, select,
+        SUCCESS {{playerName} a réussi l’action « {actionName} »}
+        PARTIAL {{playerName} a partiellement réussi l’action « {actionName} »}
+        FAILURE {{playerName} a échoué dans l’action « {actionName} »}
+        ITEM_MISSING {Il manque un objet à {playerName} pour réaliser l’action « {actionName} »}
         other {Je ne sais pas si {playerName} a réussi l’action « {actionName} »}
+    }',
+    'simple action status' => '{status, select,
+        SUCCESS {L’action a réussi}
+        PARTIAL {L’action a partiellement réussi}
+        FAILURE {L’action a échoué}
+        ITEM_MISSING {Il manque un objet pour réaliser l’action}
+        other {Je ne sais pas si l’action a réussi ou non}}
     }',
     'gained item' => 'Tu as maintenant un(e) {itemName} dans ton sac à dos',
 ];
